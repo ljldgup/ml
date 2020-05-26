f379e06ae31fbc8f6b7820f06c2ad90b33021b75
ljldgup <1142509832@qq.com>
2020-05-26 21:08:53 +0800

    分类问题

---
+++ sklearn/minst_classification.py
@@ -0,0 +1,166 @@
+import os
+
+from sklearn import clone
+from sklearn.datasets import fetch_openml
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score, cross_val_predict
+from sklearn.base import BaseEstimator
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, precision_recall_curve, roc_curve, \
+    roc_auc_score
+from sklearn.metrics import f1_score
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+# HTTP_PROXY = "http_proxy"
+# HTTPS_PROXY = "https_proxy"
+# os.environ[HTTP_PROXY] = "cn-proxy.jp.oracle.com:80"
+# os.environ[HTTPS_PROXY] = "cn-proxy.jp.oracle.com:80"
+mnist = fetch_openml('mnist_784', version=1)
+print(mnist.keys())
+
+X, y = mnist["data"], mnist["target"]
+print(X.shape)
+print(y.shape)
+
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28, 28)
+plt.imshow(some_digit_image, cmap="binary")
+plt.axis("off")
+plt.show()
+
+print(y[0])
+y = y.astype(np.uint8)
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# 这里吧0-9的分类，转化成==5，！=5的二分类
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+# 随机梯度下降分类器并不是一个独立的算法，而是一系列利用随机梯度下降求解参数的算法的集合。
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42)
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+
+# 即使全部返回0，也有90的正确率
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+
+never_5_clf = Never5Classifier()
+# cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+# 行序号代表的实际类别
+# 列序号是代表的分类器分类结果
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+confusion_matrix(y_train_5, y_train_pred)
+confusion_matrix(y_train_5, y_train_5)
+
+precision_score(y_train_5, y_train_pred)
+recall_score(y_train_5, y_train_pred)
+
+# f1 = 2 / (1/precision + 1/recall)
+f1_score(y_train_5, y_train_pred)
+
+# 随着阀值thresholds的变动，precisions，recalls发生相应的变化
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+
+
+# highlight the threshold and add the legend, axis label, and grid
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')  # Dashed diagonal
+
+
+plot_roc_curve(fpr, tpr)
+plt.show()
+
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+
+y_scores_forest = y_probas_forest[:, 1]  # score = proba of positive class
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+plt.plot(fpr, tpr, "b:", label="SGD")
+plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+plt.legend(loc="lower right")
+plt.show()
+
+# 使用auc衡量
+roc_auc_score(y_train_5, y_scores_forest)
+
+# 多分类器
+ovr_clf = OneVsRestClassifier(SVC())
+ovr_clf.fit(X_train, y_train)
+ovr_clf.predict([some_digit])
+
+print(len(ovr_clf.estimators_))
+
+sgd_clf.fit(X_train, y_train)
+sgd_clf.predict([some_digit])
+sgd_clf.decision_function([some_digit])
+
+cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy")
+
+# 缩放后再处理，准确度提高
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
+cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy")
+
+# 多分类的
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+# 多分类混淆矩阵及confusion_matrix
+conf_mx = confusion_matrix(y_train, y_train_pred)
+plt.matshow(conf_mx, cmap=plt.cm.gray)
+plt.show()
+
+row_sums = conf_mx.sum(axis=1, keepdims=True)
+norm_conf_mx = conf_mx / row_sums
+np.fill_diagonal(norm_conf_mx, 0)
+plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+plt.show()
+
+# 多标签分类
+y_train_large = (y_train >= 7)
+y_train_odd = (y_train % 2 == 1)
+y_multilabel = np.c_[y_train_large, y_train_odd]
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_multilabel)
+y_train_knn_pred = cross_val_predict(knn_clf, X_train, y_multilabel, cv=3)
+f1_score(y_multilabel, y_train_knn_pred, average="macro")
