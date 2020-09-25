8e4ae268f62f8eb0255f934c01318c5a3e342946
ljldgup <1142509832@qq.com>
2020-09-25 18:39:28 +0800

    -

--- mine/housing_price.py
+++ mine/housing_price.py
@@ -3,19 +3,43 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from scipy.stats import norm
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
 from scipy import stats
 import warnings
 
-warnings.filterwarnings("ignore")
+from sklearn.svm import SVR, LinearSVR
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
+warnings.filterwarnings("ignore")
+'''
+过程思路首选观察数据，通过直觉判断哪些列有影响，然后通过散点图，箱型图等确定是否存在明显关系
+研究目标变量的分布，峰度，偏度
+
+查看相关性热力图，确定与目标变量相关性较大的变量，同时观察非目标变量中是否存在相关性较大的特征，判断是否应该只保留一个
+查看每队因素的散点图，查找是否有忽略的关系。
+
+缺失数据：
+缺失较多，缺失数据与目标变量基本无关的直接删除
+缺失较少，缺失数据与目标变量基本有关的可以考虑直接删除样本
+
+通过可视化查看离群值，删除明显与趋势不符合的点
+
+对重要的连续值进行线性变化，使其更加符合正态分布
+
+
+'''
 df_train = pd.read_csv("train_housin.csv")
 
+'''
 # 共有81列，除去价格仍然有80各特征
 df_train.columns
 df_train.info()
 
-'''
+
 # 右偏分布（也叫正偏分布） 分位数特征也可以看出来
 sns.distplot(df_train['SalePrice'])
 df_train['SalePrice'].describe()
@@ -29,11 +53,11 @@ print("Kurtosis: %f" % df_train['SalePrice'].kurt())
 # GrLivArea 和 SalePrice呈线性关系
 var = 'GrLivArea'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000));
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
 
 var = 'TotalBsmtSF'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000));
+data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
 
 # 线性或指数关系，波动较大
 var = 'YearBuilt'
@@ -78,11 +102,14 @@ hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'
                  xticklabels=cols.values)
 plt.show()
 
-# 相关性的散点图
+# 每队特征散点图
+#  'TotalBsmtSF' 'GrLiveArea' 有线性特征
+# 'SalePrice' 'YearBuilt' 的关系比箱型图更加明显
 sns.set()
 cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
 sns.pairplot(df_train[cols], size=2.5)
 plt.show()
+'''
 
 # 缺失数据统计,聚合操作sum后列转到了索引上
 total = df_train.isnull().sum().sort_values(ascending=False)
@@ -96,7 +123,6 @@ df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index, 1)
 df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
 # 确定没有缺失
 df_train.isnull().sum().max()
-'''
 
 '''
 # 对售价进行进一步分析，标准化后，查看前十个和最后十个数据，最后是个数据离散很严重
@@ -113,16 +139,20 @@ print(high_range)
 var = 'GrLivArea'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+'''
+
 # 图上右下方售价最高的两个点是离群点，删掉
 delete_index = df_train.sort_values(by='GrLivArea', ascending=False)[:2].index
 df_train = df_train.drop(index=delete_index)
 
+'''
 # 没什么问题
 var = 'TotalBsmtSF'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
 '''
 
+'''
 # Histogram 画出峰度，偏度
 # probplot 根据分位数据推测其正态分布并与数据画在一起，横坐标是理论分位数，0的地方应该是均值
 # 理论分位数应该给出的是20%，40%，60%，80%，100%的分位数，但是100%的分位数一般会无限大，因此在这里需要进行一下 数据处理 ，找一个“近似”的理论样本，来替代“真正”的理论样本
@@ -130,9 +160,11 @@ sns.distplot(df_train['SalePrice'], fit=norm)
 fig = plt.figure()
 # 理论分布和实际分布很明显不吻合
 res = stats.probplot(df_train['SalePrice'], plot=plt)
+'''
 
 # 对SalePrice取对数后很明显数据和正态分布的吻合度更高，可以将此作为标准化后的数据
 df_train['SalePrice'] = np.log(df_train['SalePrice'])
+'''
 sns.distplot(df_train['SalePrice'], fit=norm)
 fig = plt.figure()
 res = stats.probplot(df_train['SalePrice'], plot=plt)
@@ -141,20 +173,22 @@ res = stats.probplot(df_train['SalePrice'], plot=plt)
 sns.distplot(df_train['GrLivArea'], fit=norm);
 fig = plt.figure()
 res = stats.probplot(df_train['GrLivArea'], plot=plt)
+'''
 
 df_train['GrLivArea'] = np.log(df_train['GrLivArea'])
+'''
 sns.distplot(df_train['GrLivArea'], fit=norm);
 fig = plt.figure()
 res = stats.probplot(df_train['GrLivArea'], plot=plt)
+'''
 
 # HasBsmt 对于=0的值取1，然后再取log
 # 这里len(df_train['TotalBsmtSF'])实际上只是这一列的值
 df_train['HasBsmt'] = pd.Series(len(df_train['TotalBsmtSF']), index=df_train.index)
 df_train['HasBsmt'] = 0
 df_train.loc[df_train['TotalBsmtSF'] > 0, 'HasBsmt'] = 1
-
 df_train.loc[df_train['HasBsmt'] == 1, 'TotalBsmtSF'] = np.log(df_train['TotalBsmtSF'])
-
+'''
 sns.distplot(df_train[df_train['TotalBsmtSF'] > 0]['TotalBsmtSF'], fit=norm);
 fig = plt.figure()
 res = stats.probplot(df_train[df_train['TotalBsmtSF'] > 0]['TotalBsmtSF'], plot=plt)
@@ -162,6 +196,38 @@ res = stats.probplot(df_train[df_train['TotalBsmtSF'] > 0]['TotalBsmtSF'], plot=
 # 进行变换后从三点图上可以看出，线性关系更强了
 plt.scatter(df_train['GrLivArea'], df_train['SalePrice'])
 plt.scatter(df_train[df_train['TotalBsmtSF'] > 0]['TotalBsmtSF'], df_train[df_train['TotalBsmtSF'] > 0]['SalePrice'])
-
+'''
 # 类转换成onehot
-df_train = pd.get_dummies(df_train)
+# df_train = pd.get_dummies(df_train)
+X = df_train.drop(columns='SalePrice').values
+y = df_train['SalePrice'].values
+
+df_test = pd.read_csv('test_housing.csv')
+
+# 有不少列缺失值
+df_test.dtypes[df_test.isna().sum() > 0]
+df_test.isna().sum()[df_test.isna().sum() > 0]
+
+df_test['GrLivArea'] = np.log(df_test['GrLivArea'])
+df_test = df_test.drop((missing_data[missing_data['Total'] > 1]).index, 1)
+df_test['HasBsmt'] = pd.Series(len(df_test['TotalBsmtSF']), index=df_test.index)
+df_test['HasBsmt'] = 0
+df_test.loc[df_test['TotalBsmtSF'] > 0, 'HasBsmt'] = 1
+df_test.loc[df_test['HasBsmt'] == 1, 'TotalBsmtSF'] = np.log(df_test['TotalBsmtSF'])
+
+'''
+regressors = [LinearSVR(C=1), SVR(kernel="rbf", C=1),
+              KNeighborsRegressor(n_neighbors=6),
+              RandomForestRegressor(),
+              GradientBoostingRegressor()]
+
+for regressor in regressors:
+    print(regressor)
+    y_pred = regressor.fit(X, y)
+    y_pred = regressor.predict(X)
+    # 随机打印20个样本及预测值，误差
+    idx = np.random.randint(0, len(y) - 1, size=20)
+    print(np.c_[np.exp(y),np.exp(y_pred), np.exp(y_pred) - np.exp(y)][idx, :])
+    print(mean_squared_error(y_pred, y))
+    print(cross_val_score(regressor, X, y, cv=3, scoring="neg_mean_squared_error"))
+'''
--- numpy_pandas/seaborn_basic.py
+++ numpy_pandas/seaborn_basic.py
@@ -14,25 +14,87 @@ sns.distplot(x, kde=True)
 # 调节bins，对数据更具体的做分桶操作。
 sns.distplot(x, kde=True, bins=20)
 
+# hist直方图,dist是根据密度绘制
+# 效果基本一样，但很纵坐标有区别，dist更快
+sns.histplot(x, kde=True, bins=20)
+
 # rug生成实例
 sns.distplot(x, kde=False, bins=20, rug=True)
 
-# 核密度估计
+# 更具分布类型绘制
+sns.distplot(np.random.randn(100), fit=stats.norm)
+sns.distplot(np.exp(np.random.randn(100)), fit=stats.norm)
+
+x = np.random.gamma(6, size=200)
+sns.distplot(x, kde=False, fit=stats.gamma)
+
+# 核密度估计绘制
 sns.kdeplot(x)
 
-# bandwith
+# bandwith，用于近似的正态分布曲线的宽度
 sns.kdeplot(x)
 sns.kdeplot(x, bw=.2, label="bw: 0.2")
 sns.kdeplot(x, bw=2, label="bw: 2")
 plt.legend()
-
-x = np.random.gamma(6, size=200)
-sns.distplot(x, kde=False, fit=stats.gamma)
 
 # 双变量分布
 mean, cov = [0, 1], [(1, .5), (.5, 1)]
 data = np.random.multivariate_normal(mean, cov, 200)
 df = pd.DataFrame(data, columns=["x", "y"])
 
-# 散点图
+# 散点图+柱状图
 sns.jointplot(x="x", y="y", data=df)
+
+# 六角形箱
+x, y = np.random.multivariate_normal(mean, cov, 1000).T
+with sns.axes_style("ticks"):
+    sns.jointplot(x=x, y=y, kind="hex")
+
+# 核密度估计
+sns.jointplot(x="x", y="y", data=df, kind="kde")
+f, ax = plt.subplots(figsize=(6, 6))
+sns.kdeplot(df.x, df.y, ax=ax)
+sns.rugplot(df.x, color="g", ax=ax)
+sns.rugplot(df.y, vertical=True, ax=ax)
+
+# 散点图 + 线性回归 + 95%置信区间
+x = np.random.randint(10, 20, size=(30, 1))
+y = x * 2
+cond = np.random.randint(10, 20, size=(30, 1)) % 2
+data = pd.DataFrame(np.c_[x + np.random.randn(30, 1), y + np.random.randn(30, 1), cond],
+                    columns=['x', 'y', 'cond'])
+# 属性名 + dataframe
+sns.lmplot(x="x", y="y", data=data)
+# 根据条件分开拟合
+sns.lmplot(x="x", y="y", hue='cond', data=data)
+# size控制图片大小
+sns.lmplot(x="x", y="y", data=data, size=6)
+
+# 箱型图
+x = np.random.randint(10, 20, size=(100, 1))
+y = x % 5
+data = pd.DataFrame(np.c_[x, y + np.random.randn(100, 1)], columns=['x', 'y'])
+sns.boxplot(x="x", y="y", data=data)
+
+# 柱状图,中间黑线误差棒 注明所测量数据的不确定度的大小
+sns.set(style="whitegrid", color_codes=True)
+titanic = pd.read_csv('../mine/train_titanic.csv')
+# 统计柱状图
+sns.barplot(x="Sex", y="Survived", hue="Pclass", data=titanic)
+sns.barplot(x="Sex", y="Pclass", hue="Survived", data=titanic)
+# 灰度柱状图，不要y参数
+sns.countplot(x="Sex", hue="Pclass", data=titanic)
+
+# 分类散点图，一维数据是分类数据时，散点图成为了条带形状
+sns.stripplot(x="Pclass", y="Fare", data=titanic)
+# 去除抖动后是几条线
+sns.stripplot(x="Pclass", y="Fare", data=titanic, jitter=False)
+
+# 蜂窝图
+sns.swarmplot(x="Pclass", y="Fare", data=titanic)
+
+# 两两作图
+sns.pairplot(data=titanic[['Survived', 'Pclass', 'Fare']])
+
+# 热力图
+sns.heatmap(titanic.corr(), fmt='.2f', annot=True)
